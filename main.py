8e216391b35125ef43f29e53e35e5d51f4a66ece
Yonathan Desalegn <167541328+yonathanim@users.noreply.github.com>
2026-04-07 17:56:47 +0300

    feat: basic merge implementatiom)

--- main.py
+++ main.py
@@ -261,6 +261,28 @@ def create_branch(name):
 
     print(f"Branch '{name}' created at {current_commit}")
 
+def merge_branch(name):
+    branch_path = os.path.join(".snapgit", "refs", "heads", name)
+
+    if not os.path.exists(branch_path):
+        print("Branch not found")
+        return
+
+    current_commit = get_current_commit()
+
+    with open(branch_path, "r") as f:
+        other_commit = f.read().strip()
+
+    if current_commit == other_commit:
+        print("Already up to date")
+        return
+
+    # Read other commit files (simple strategy: take their version)
+    path = os.path.join(".snapgit", "objects", other_commit)
+
+    with open(path, "rb") as f:
+        data = f.read()
+
 # --------------------
 # CLI
 # --------------------
@@ -302,8 +324,12 @@ if __name__ == "__main__":
                 print("Provide a branch name")
             else:
                 create_branch(sys.argv[2])
+        elif command == "merge":
+            if len(sys.argv) < 3:
+                print("Provide branch name")
+            else:
+                merge_branch(sys.argv[2])        
         elif command == "log":        
             log_commits()                           
         else:
             print("Unknown command")
-        
